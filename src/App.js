d3da2302b9c0679f38132ae653a8b483f2f00392
Itzamna Huerta <itzamnahuerta@Itzamnas-MacBook-Pro.local>
2023-05-01 11:59:07 -0400

    removed router code comments

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
-// import { HashLink as Link } from 'react-router-hash-link';
 import './App.scss';
 import About from './components/About.jsx';
-import BarChart from './components/BarChart'
 import Title from './components/Title';
-
 import Methodology from './components/Methodology';
 
 function App() {
-
-
   return (
     <section>
       <div className="progress"></div>
       <Title/>
       <About/>
-      {/* <BarChart/> */}
       <Methodology/>
-      
-      {/* <Routes>
-        <Route path='/home' element={<Title/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/ny_state_leg' element={<BarChart/>} />
-      </Routes> */}
-
     </section>
   );
 }
